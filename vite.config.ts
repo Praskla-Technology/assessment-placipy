452626f9d9e8302f5144a063c752f719f7854262
Monal <monalprathaps@gmail.com>
2026-01-01 12:54:43 +0530

    revert vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    server: {
-      port: 5173,
-      proxy: {
-        '/api': {
-          target: env.VITE_URL || 'http://localhost:3000',
-          changeOrigin: true,
-          secure: false,
-          ws: true,
-        }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+         target: process.env.VITE_URL,
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        
       }
     }
   }
